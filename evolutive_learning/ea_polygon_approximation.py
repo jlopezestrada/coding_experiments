c24993cb93a8d4da80cc64e5614b047a99bb71cb
Jose Angel Lopez <jose.a.l.e@hotmail.com>
2026-01-18 13:53:45 +0100

    feat(ea): add general information in polygon approximation

--- evolutive_learning/ea_polygon_approximation.py
+++ evolutive_learning/ea_polygon_approximation.py
@@ -1,1 +1,10 @@
 # Polygon Approximation using evolutive learning
+# Goal: find all pixels in the new image to be the same as the reference image
+# Individual = a list of N shapes
+# Population = Set of Individuals
+# Genotype = R, G, B, alpha
+# Search Space = todo
+# Operators
+#    Selection = Selects a number that would continue the generations
+#    Crossover = Perform an operation with another number
+#    Mutation = Perform an operation with a random number
